7f6660337ae471ca736da532f7a828e18d02bc13
HUAWEI <123>
2019-02-28 18:34:30 +0800

    知识体系

--- app/src/main/java/com/jy/theplayandroid/playandroid/playandroid/zhishitixi/ZhishitixiFragment.java
+++ app/src/main/java/com/jy/theplayandroid/playandroid/playandroid/zhishitixi/ZhishitixiFragment.java
@@ -1,12 +1,12 @@
 package com.jy.theplayandroid.playandroid.playandroid.zhishitixi;
 
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +26,7 @@ import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
  * A simple {@link Fragment} subclass.
  */
 public class ZhishitixiFragment extends Fragment {
+
 
 
     private SmartRefreshLayout Smart;
