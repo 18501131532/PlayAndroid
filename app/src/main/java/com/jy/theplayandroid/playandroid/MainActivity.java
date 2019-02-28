c3c6f1bc542c6a9b48da6034a58f4e5d9a81ea88
guozhijun1999 <846606492@qq.com>
2019-02-28 09:01:47 +0800

    郭志俊1111

--- app/src/main/java/com/jy/theplayandroid/playandroid/MainActivity.java
+++ app/src/main/java/com/jy/theplayandroid/playandroid/MainActivity.java
@@ -15,7 +15,7 @@ import com.jy.theplayandroid.playandroid.presenter.TalkClassiyPresenter;
 import okhttp3.FormBody;
 
 public class MainActivity extends BaseActivity<TalkClassify.TalkClassifyView,TalkClassiyPresenter<TalkClassify.TalkClassifyView>> implements TalkClassify.TalkClassifyView{
-
+    //郭志俊第一次上传=====================
 
     @Override
     public void showLoading() {
