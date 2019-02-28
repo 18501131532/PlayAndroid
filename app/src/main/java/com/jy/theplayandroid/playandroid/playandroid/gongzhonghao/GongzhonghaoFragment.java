e59d8446d70efa0d1b1c17df0c68900140347ead
duanxqing <2045538093@qq.com>
2019-02-28 09:58:20 +0800

    老段第二次修改

---
+++ app/src/main/java/com/jy/theplayandroid/playandroid/playandroid/gongzhonghao/GongzhonghaoFragment.java
@@ -0,0 +1,30 @@
+package com.jy.theplayandroid.playandroid.playandroid.gongzhonghao;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jy.theplayandroid.playandroid.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GongzhonghaoFragment extends Fragment {
+
+
+    public GongzhonghaoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gongzhonghao, container, false);
+    }
+
+}
