c0fb30e2243bc5ad01ce68a9b2b6930577b0c258
HUAWEI <123>
2019-03-01 21:09:51 +0800

    知识体系

--- app/src/main/java/com/jy/theplayandroid/playandroid/http/ApiServer.java
+++ app/src/main/java/com/jy/theplayandroid/playandroid/http/ApiServer.java
@@ -2,6 +2,8 @@ package com.jy.theplayandroid.playandroid.http;
 
 import com.jy.theplayandroid.playandroid.playandroid.xiangmu.bean.ProjectClassifyData;
 import com.jy.theplayandroid.playandroid.playandroid.xiangmu.bean.ProjectListBean;
+import com.jy.theplayandroid.playandroid.playandroid.zhishitixi.bean.OneBean;
+import com.jy.theplayandroid.playandroid.playandroid.zhishitixi.bean.TwoBEAN;
 
 import io.reactivex.Observable;
 import okhttp3.RequestBody;
@@ -27,5 +29,8 @@ public interface ApiServer {
 
     @GET("tree/json")
     Observable<OneBean> getZhishiOne();
+
+    @GET("article/list/{curPage}/json")
+    Observable<TwoBEAN> getZhishiTwo(@Path("curPage")String page, @Query("chapterId") String id);
 
 }
